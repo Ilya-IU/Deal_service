a8ce205a2fa2c063bda87c91e6f29a8bf98f323c
Ilya-IU <ustyugov-1999@bk.ru>
2024-12-02 18:31:09 +0500

    init_2Mvp

--- src/main/java/com/example/services/CalculateScoringServiceImpl.java
+++ src/main/java/com/example/services/CalculateScoringServiceImpl.java
@@ -10,6 +10,7 @@ import com.example.entity.Statement;
 import com.example.feignClient.CalculateClient;
 import com.example.repository.StatementRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,7 @@ import static com.example.enums.ChangeType.AUTOMATIC;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CalculateScoringServiceImpl implements CalculateScoringService {
     @Autowired
     private StatementRepository statementRepository;
@@ -30,10 +32,15 @@ public class CalculateScoringServiceImpl implements CalculateScoringService {
 
     @Override
     public void ScoringCalculateOffer(FinishRegistrationRequestDto requestDto, Long statementId) {
+        log.info("Поиск заявки по id, с параметрами FinishRegistrationRequestDto = {}, statementid = {}", requestDto, statementId);
         Statement statement = statementRepository.findById(statementId).orElse(null);
+        log.info("Заявка найдена statement = {}", statement);
         Client client = statement.getClient();
+        log.info("Клинет найден statement = {}", client);
         Credit credit = statement.getCredit();
+        log.info("Кредит найден statement = {}", credit);
         ScoringDataDto scoringDataDto = createScoringDataDto(requestDto, statement, client, credit);
+        log.info("Запуск сервиса Calculate для расчета параметров скоринга scoringDataDto = {}", scoringDataDto);
         calculateClient.calculateCreditParams(scoringDataDto);
 
     }
@@ -41,6 +48,7 @@ public class CalculateScoringServiceImpl implements CalculateScoringService {
 
     private ScoringDataDto createScoringDataDto(FinishRegistrationRequestDto requestDto,
                                                 Statement statement, Client client, Credit credit) {
+        log.info("Наполнение scoringDataDto");
         ScoringDataDto scoringDataDto = new ScoringDataDto();
 
         scoringDataDto.setAmount(credit.getAmount());
@@ -64,13 +72,16 @@ public class CalculateScoringServiceImpl implements CalculateScoringService {
         //бд->
         scoringDataDto.setIsInsuranceEnabled(statement.getAppliedOffer().getIsInsuranceEnabled());
         scoringDataDto.setIsSalaryClient(statement.getAppliedOffer().getIsSalaryClient());
+        log.info("scoringDataDto заполнено параметрами ={}", scoringDataDto);
         statement.setStatus(CC_APPROVED);
         List<StatementStatusHistoryDto> historyStatuses = statement.getStatementStatusHistoryDto();
         historyStatuses.add(new StatementStatusHistoryDto( CC_APPROVED, LocalDate.now(), AUTOMATIC));
+        log.info("История статусов заявки в бд historyStatuses = {}", historyStatuses);
         statement.setStatementStatusHistoryDto(historyStatuses);
+        log.info("Обновление статуса заявки в БД");
 
         statementRepository.save(statement);
-
+        log.info("Сохранение заявки в бд/возвращение scoringDataDto");
         return scoringDataDto;
     }
 }
